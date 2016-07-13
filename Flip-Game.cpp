ec28ec39f04bf5024c41705e2f8d75571dd7bc1f
RainbowSecret <PkuRainBow@users.noreply.github.com>
2016-07-13 21:14:25 +0800

    Update Flip-Game.cpp

--- Flip-Game.cpp
+++ Flip-Game.cpp
@@ -29,3 +29,19 @@ public:
 
     }
 };
+
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string s) {
+        vector<string> moves;
+        int n = s.length();
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == '+' && s[i + 1] == '+') {
+                s[i] = s[i + 1] = '-';
+                moves.push_back(s);
+                s[i] = s[i + 1] = '+';
+            }
+        }
+        return moves;
+    }
+};
